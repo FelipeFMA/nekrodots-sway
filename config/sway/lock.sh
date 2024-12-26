3f248b5b02b4aa452d6f9b437a798cc294eb06c7
FelipeFMA <felipefmavelar@gmail.com>
2024-12-26 20:52:53 -0300

    added a polkit and fixed xdg-portal stuff

---
+++ config/sway/lock.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+swaylock \
+  --indicator-radius 100 \
+  --indicator-thickness 20 \
+  --inside-color 1b1b1b \
+  --inside-clear-color 1b1b1b \
+  --inside-ver-color 1b1b1b \
+  --inside-wrong-color 1b1b1b \
+  --key-hl-color ffffff \
+  --bs-hl-color 000000 \
+  --ring-color 000000 \
+  --ring-clear-color 000000 \
+  --ring-wrong-color 000000 \
+  --ring-ver-color 000000 \
+  --line-color 000000 \
+  --font 'Roboto' \
+  --text-color ffffff \
+  --text-clear-color ffffff \
+  --text-wrong-color ff0000 \
+  --text-ver-color ffffff \
+  --image ~/Pictures/Wallpapers/leaves.jpg \
+  --clock \
+  --indicator \
+  --timestr '%I:%M %p' \
+  --datestr %d/%m/%Y \
+ 
